306845010ce790ba3b2e553780f3ad5cc72b4f29
CompSciHigh24 <csh-github@compscihigh.org>
2024-09-05 15:35:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#addtocart")
+console.log(btn)
+btn.addEventListener("click", e =>{
+  console.log("Hello World")
+})
+
+let btn2 = document.querySelector("#addtocart2") 
+  console.log(btn)
+btn.addEventListener("click", e =>{
+  console.log("Hello World")
+})
+
